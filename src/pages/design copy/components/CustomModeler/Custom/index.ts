897d568f666e6ca5841f2e9397e700dd523cbbf9
laism <laism@chinacsci.com>
2020-09-21 15:40:18 +0800

    删除备份文件

--- src/pages/design copy/components/CustomModeler/Custom/index.ts
+++
@@ -1,7 +0,0 @@
-import CustomPalette from './CustomPalette'
-import CustomContextPadProvider from './CustomContextPadProvider'
-export default {
-  __init__: ['paletteProvider', 'contextPadProvider'],
-  paletteProvider: ['type', CustomPalette],
-  contextPadProvider: ['type', CustomContextPadProvider]
-}
--- src/pages/design copy/components/XmlHub.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-export const XmlContent = React.createContext({
-  xml: '',
-  setXml: () => null,
-})
